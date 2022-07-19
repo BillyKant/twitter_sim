975a57ae571b53d15bc510c1e283122ffa510269
BillyKant <105659514+BillyKant@users.noreply.github.com>
2022-07-19 07:50:28 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+
+app.listen(port, () => {
+    console.log(`Hello World app listening on port ${port}`)
+    }) 
+
+app.set('view engine', 'ejs')
+
+const user = {
+    firstName: 'Billy',
+    lastName: 'Kantor'
+}
+
+app.get('/', (req, res) => {
+	res.render('pages/index', {user:user})
+})
+
+app.get('/profile', (req, res) => {
+	res.render('pages/profile', {user:user})
+})
+
+app.get('/user', (req, res) => {
+	res.render('pages/user', {user:user})
+})
+
+app.get('/login', (req, res) => {
+	res.render('pages/login', {user:user})
+})
+
+app.get('/register', (req, res) => {
+	res.render('pages/register', {user:user})
+})
+
+//Middleware
+app.use((req,res,next)=>{
+    console.log("Current Time :", Date())
+    next()
+}, (req,res,next)=>{
+    console.log('This is the next function called above')
+    next()
+})
+
+app.use(express.static(path.join(__dirname, 'public')))
