2e815966e47ee069f8247b2dcf840471ac16a3ac
BillyKant <105659514+BillyKant@users.noreply.github.com>
2022-07-20 10:17:02 -0400

    added queries

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,3 +1,19 @@
 const { GraphQLList, GraphQLID, GraphQLString } = require('graphql')
 const { UserType, PostType } = require('./types')
 const { User, Post } = require('../models')
+
+const users = {
+    type: new GraphQLList(UserType),
+    description: 'Query all users in database',
+    resolve(parent, args){
+        return User.find()
+    }
+}
+
+const posts = {
+    type: new GraphQLList(UserType),
+    description: 'Query all users in database',
+    resolve(parent, args){
+        return Post.find()
+    }
+}
